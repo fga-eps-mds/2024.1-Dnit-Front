55ad8afafe0fc2ac32c153a63c7229be693fb697
Wagner M Cunha <wagnermc506@gmail.com>
2023-11-11 16:48:30 -0300

    chore: ajustando fechamento de modal

--- src/components/EditarEmpresaDialog/index.tsx
+++ src/components/EditarEmpresaDialog/index.tsx
@@ -2,27 +2,31 @@ import { useState } from "react";
 import Modal from "../Modal";
 // import "./styles.css"
 
-export default function EditarEmpresasDialog( { closeDialog } ) {
+interface EditarEmpresasDialogProps {
+	closeDialog: () => void;
+}
+
+export default function EditarEmpresasDialog( { closeDialog }: EditarEmpresasDialogProps ) {
 	return (
 		<Modal className="">
 			<div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
 				<h4 className="text-center mt-1">Editar Empresa</h4>
-				<button data-testid="botaoFechar" className="br-button close circle" type="button" aria-label="Close">
+				<button data-testid="botaoFechar" className="br-button close circle" type="button" aria-label="Close" onClick={() =>{ closeDialog() }}>
 					<i className="fas fa-times" aria-hidden="true"></i>
 				</button>
       		</div>
 			<div style={ {height: "inherit"} }>
 				<div className="br-input">
-					<label>Label</label>
+					<label>Razão Social</label>
 					<input id="input-default" type={"text"} placeholder="Placeholder"/>
 				</div>
 				<div className="br-input">
-					<label>Label</label>
+					<label>CNPJ</label>
 					<input id="input-default" type={"text"} placeholder="Placeholder"/>
 				</div>
 			</div>
 			<div className="d-flex w-100 justify-content-end">
-        		<button data-testid="botaoCancelar" className="br-button secondary" type="button" onClick={() => {closeDialog(false)}}>Cancelar</button>
+        		<button data-testid="botaoCancelar" className="br-button secondary" type="button" onClick={() => {closeDialog()}}>Cancelar</button>
         		<button data-testid="botaoConfirmar" className="br-button primary" type="button">Confirmar</button>
       		</div>
 		</Modal>
