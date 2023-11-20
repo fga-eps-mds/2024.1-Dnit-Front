8f914bda1187e19de66005429b434b8de321d593
Wagner M Cunha <wagnermc506@gmail.com>
2023-11-19 23:06:02 -0300

    fix: remove comentários desnecessários

--- src/tests/components/AdicionarUsuarioDialog.spec.tsx
+++ src/tests/components/AdicionarUsuarioDialog.spec.tsx
@@ -1,15 +1,7 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/render-result-naming-convention */
 import { render, waitFor, act, fireEvent } from "@testing-library/react";
-// import { Permissao } from "../models/auth";
-// import { autenticar } from "./mock/autenticacao";
 import server from "../mock/servicosAPI";
-// import { MemoryRouter, Route, Routes } from "react-router-dom";
-// import { AuthProvider } from "../provider/Autenticacao";
-// import GerenciarUsuariosEmpresa from "../pages/gerencia/GerenciarUsuariosEmpresa";
-// import { empresas } from "./stub/empresaModelos";
-// import { usuarios } from "./stub/usuarioModelos";
-import MultiSelect from "../../components/MultiSelect";
 import AdicionarUsuarioDialog from "../../components/AdicionarUsuarioDialog";
 import { empresas } from "../stub/empresaModelos";
 
