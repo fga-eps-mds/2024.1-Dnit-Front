74eb3019f2a161bdf8e39b08ebe37f3ee76f468e
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-11-22 11:19:23 -0300

    feat: cria dialogo para ver detalhes da solicitação de ação

---
+++ src/components/SolicitacoesDialog/index.tsx
@@ -0,0 +1,142 @@
+import React, { ReactNode, useState, useEffect } from 'react';
+import "../../styles/App.css";
+import "../../pages/Ranque/";
+import Modal from "../../components/Modal/index";
+import { fetchEscolaRanque } from '../../service/ranqueApi';
+import { fetchSuperintendenciaData } from '../../service/escolaApi';
+import { EscolaRanqueDetalhes } from '../../models/ranque';
+import ReactLoading from "react-loading";
+import { Superintendencia } from '../../models/service';
+import { formataCustoLogistico } from '../../utils/utils';
+import "./index.css";
+
+interface ModalProps {
+    onClose: () => void;
+    onCreateAcao: () => void;
+    escolaId?: string;
+}
+
+interface LabelProps {
+    children: ReactNode,
+    className?: string,
+}
+
+function Label({ children, className }: LabelProps) {
+    return (<label style={{ fontSize: '14px', fontWeight: 'normal' }} className={'mb-2 ' + className}>
+        {children}
+    </label>)
+}
+
+const SolitacoesDialog: React.FC<ModalProps> = ({ escolaId, onClose, onCreateAcao }) => {
+
+    const [escolaSelecionada, setEscolaSelecionada] = useState<EscolaRanqueDetalhes | null>(null);
+    const [superintendenciaSelecionada, setSuperintendenciaSelecionada] = useState<Superintendencia | undefined>();
+
+    const fetchSuperintendenciaSelecionada = async (superintendenciaId?: number) => {
+        const superintendencia = await fetchSuperintendenciaData(superintendenciaId);
+        setSuperintendenciaSelecionada(superintendencia);
+    }
+
+    // useEffect(() => {
+    //     fetchEscolaRanque(escolaId)
+    //         .then((escola) => {
+    //             setEscolaSelecionada(escola);
+    //             fetchSuperintendenciaSelecionada(escola.superintendencia?.id);
+    //         }
+    //         )
+
+    // }, []);
+
+
+    // if (!escolaSelecionada) {
+    //     return (
+    //         <Modal className="modal-title" closeModal={() => onClose()}>
+    //             <h4 className="text-center mt-2">Carregando Escola...</h4>
+    //             <div className="d-flex justify-content-center m-4">
+    //                 <ReactLoading type="spinningBubbles" color="#000000" />
+    //             </div>
+    //             <span></span>
+    //         </Modal>
+    //     );
+    // }
+
+    return (
+        <Modal className="default escola-ranque-modal" closeModal={() => onClose()}>
+            <div className="d-flex flex-column">
+                <h4 className="text-center mt-1">Detalhes da Solicitação</h4>
+                <div className='d-flex flex-column '>
+                    <Label>Nome Escola: Nome</Label>
+                    <Label><strong>Dados</strong></Label>
+                    <div className='row mb-2'>
+                        <div className='col-12 col-md-6'>
+                            <Label>Código: Codigo</Label>
+                        </div>
+                        <div className='col-12 col-md-6'>
+                            <Label>Telefone escola: {"619999888".replace(/^(\d{2})(\d{4})(\d{4})/gm, "($1) $2-$3")}</Label>
+                        </div>
+                    </div>
+                    <div className='row mb-2'>
+                        <div className='col-12 col-md-6'>
+                            <Label>Alunos: numAlunos</Label>
+                        </div>
+                        <div className='col-12 col-md-6'>
+                            <Label>Professores: numProfessores</Label>
+                        </div>
+                    </div>
+                    <div className='row mb-2'>
+                        <div className='col-12 col-md-6'>
+                            <Label>Porte: Porte</Label>
+                        </div>
+                        <div className='col-12 col-md-6'>
+                            <Label>Situação: Situacao</Label>
+                        </div>
+                    </div>
+                    <Label>Rede: Rede</Label>
+                    <Label>Etapas de Ensino: Etapas</Label>
+                    <hr />
+                    <div>
+                        <div className='d-flex flex-column'>
+                            <Label><strong>Endereço</strong></Label>
+                            <Label>Endereco</Label>
+                            <Label>Cep: Cep{/*escolaSelecionada.cep.replace(/^(\d{5})(\d{3})$/gm, "$1-$2")*/}</Label>
+                            <div className='row'>
+                                <div className='col-12 col-md-6'>
+                                    <Label>Estado: UF</Label>
+                                </div>
+                                <div className='col-12 col-md-6'>
+                                    <Label>Município: Municipio</Label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className='d-flex flex-column '>
+                        <Label><strong>Solicitações</strong></Label>
+                        <div className='d-flex flex-column'>
+                            <Label>Nome do Solicitante: Fulano</Label>
+                            <Label>Vinculo com a Escola: Professor</Label>
+                            <Label>Nome do Solicitante: Fulano</Label>
+                            <Label>e-mail: fulano@gmail.com</Label>
+                            <Label>Telefone: {"619944888".replace(/^(\d{2})(\d{4})(\d{4})/gm, "($1) $2-$3")}</Label>
+                            <Label>Observações: Sem pensar conquistamos o mundo geral e construímos o nosso pequeno
+                                 lugar, deixando brilhar cada estrelinha. Estrelinhas. Doces, sensíveis, frias,
+                                  ternurentas. Mas sempre presentes em qualquer parte. Os donos da amizade.</Label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <div className="d-flex w-100 justify-content-end mb-2">
+                <button className="br-button secondary mr-3" type="button" onClick={() => onClose()}>
+                    Fechar
+                </button>
+                <button className="br-button primary mr-3" type="button" onClick={() => { onCreateAcao() }} disabled>
+                    Criar Ação
+                </button>
+            </div>
+
+        </Modal>
+    );
+};
+
+export default SolitacoesDialog;
--- src/pages/gerencia/GerenciarSolicitacoes/index.tsx
+++ src/pages/gerencia/GerenciarSolicitacoes/index.tsx
@@ -18,95 +18,126 @@ import { AuthContext } from "../../../provider/Autenticacao";
 import { Permissao } from "../../../models/auth";
 import { ButtonComponent } from "../../../components/Button";
 import AdicionarUsuarioDialog from "../../../components/AdicionarUsuarioDialog";
+import { SolicitacaoDeAcaoData } from "../../../models/service";
+import { SolicitacoesData } from "../../../models/solicitacoes";
+import SolicitacoesDialog from "../../../components/SolicitacoesDialog";
 
 
 export default function GerenciarSolicitacoes() {
-    const paginas = [{ nome: "Gerenciar Solicitações de Açoes", link: "/gerenciarSolicitacaoAcao" }];
-    const [notificationApi, notificationContextHandler] = notification.useNotification();
-    const [loading, setLoading] = useState(false);
-    const [listaSolicitacoes, setListaSolicitacoes] = useState<UsuarioModel[]>([]);
-    const [escola, setEscola] = useState('');
-    const [uf, setUf] = useState('');
-    const [municipio, setMunicipio] = useState('');
-    const [listaUfs, setListaUfs] = useState<FilterOptions[]>([]);
-    const [listaMunicipios, setListaMunicipios] = useState<FilterOptions[]>([]);
-    const { temPermissao } = useContext(AuthContext);
-    const [pagina, setPagina] = useState(1);
-    const [totalPaginas, setTotalPaginas] = useState(1);
-    const [tamanhoPagina, setTamanhoPagina] = useState(10);
+  const paginas = [{ nome: "Gerenciar Solicitações de Açoes", link: "/gerenciarSolicitacaoAcao" }];
+  const [notificationApi, notificationContextHandler] = notification.useNotification();
+  const [loading, setLoading] = useState(false);
+  const [listaSolicitacoes, setListaSolicitacoes] = useState<SolicitacoesData[]>([]);
+  const [escola, setEscola] = useState('');
+  const [uf, setUf] = useState('');
+  const [municipio, setMunicipio] = useState('');
+  const [listaUfs, setListaUfs] = useState<FilterOptions[]>([]);
+  const [listaMunicipios, setListaMunicipios] = useState<FilterOptions[]>([]);
+  const { temPermissao } = useContext(AuthContext);
+  const [pagina, setPagina] = useState(1);
+  const [totalPaginas, setTotalPaginas] = useState(1);
+  const [tamanhoPagina, setTamanhoPagina] = useState(10);
+  const [solicitacaoAtual, setSolicitacaoAtual] = useState<SolicitacoesData | null>();
 
-    const buscarSolicitacoes = (proximaPagina: number, novoTamanhoPagina: number = tamanhoPagina) => {
-		// fetchSolicitacaoAcoes(proximaPagina, novoTamanhoPagina, escola, uf, municipio)
-		// 	.then(pagina => {
-		// 		setPagina(pagina)
-		// 		setListaSolicitacoes(pagina.items)
-		// 		setTamanhoPagina(pagina.itemsPorPagina)
-		// 	})
-		// 	.catch(error => notificationApi.error({ message: 'Falha na listagem de solicitacoes. ' + (error?.response?.data || '') }))
-	}
+  const buscarSolicitacoes = (proximaPagina: number, novoTamanhoPagina: number = tamanhoPagina) => {
+    // fetchSolicitacaoAcoes(proximaPagina, novoTamanhoPagina, escola, uf, municipio)
+    // 	.then(pagina => {
+    // 		setPagina(pagina)
+    // 		setListaSolicitacoes(pagina.items)
+          // setTotalPaginas(pagina.totalPaginas)
+    // 		setTamanhoPagina(pagina.itemsPorPagina)
+    // 	})
+    // 	.catch(error => notificationApi.error({ message: 'Falha na listagem de solicitacoes. ' + (error?.response?.data || '') }))
+  }
 
-    async function fetchUf(): Promise<void> {
-        const listaUfs = await fetchUnidadeFederativa();
-        const novaUf = listaUfs.map((u) => ({ id: '' + u.id, rotulo: u.sigla }));
-        setListaUfs(novaUf);
-    }
+  async function fetchUf(): Promise<void> {
+    const listaUfs = await fetchUnidadeFederativa();
+    const novaUf = listaUfs.map((u) => ({ id: '' + u.id, rotulo: u.sigla }));
+    setListaUfs(novaUf);
+  }
 
-    async function fetchMunicipios(): Promise<void> {
-        const listaMunicipios = await fetchMunicipio(Number(uf));
-        const novoMunicipio = listaMunicipios.map((u) => ({ id: '' + u.id, rotulo: u.nome }));
-        setListaMunicipios(novoMunicipio);
-      }
+  async function fetchMunicipios(): Promise<void> {
+    const listaMunicipios = await fetchMunicipio(Number(uf));
+    const novoMunicipio = listaMunicipios.map((u) => ({ id: '' + u.id, rotulo: u.nome }));
+    setListaMunicipios(novoMunicipio);
+  }
 
-    function procuraRotuloMunicipio(usuario: UsuarioModel) {
-        return usuario.municipio === null || usuario.municipio?.nome === "undefined" ? "Não Cadastrado" : usuario.municipio?.nome;
-    }
+  function procuraRotuloMunicipio(usuario: UsuarioModel) {
+    return usuario.municipio === null || usuario.municipio?.nome === "undefined" ? "Não Cadastrado" : usuario.municipio?.nome;
+  }
 
-    function procuraRotuloUf(usuario: UsuarioModel) {
-        return listaUfs.find((uf) => uf.id === '' + usuario.ufLotacao)?.rotulo;
-    }
+  function procuraRotuloUf(solicitacao: SolicitacoesData) {
+    return listaUfs.find((uf) => uf.id === '' + solicitacao.escola.idUf)?.rotulo;
+  }
 
 
-    useEffect(() => {
-        buscarSolicitacoes(pagina, tamanhoPagina);
-    }, [escola, uf, pagina, tamanhoPagina]);
+  useEffect(() => {
+    buscarSolicitacoes(pagina, tamanhoPagina);
+  }, [escola, uf, pagina, tamanhoPagina]);
 
-    useEffect(() => {
-        fetchMunicipios();
-      }, [uf]);
+  useEffect(() => {
+    fetchMunicipios();
+  }, [uf]);
 
-    useEffect(() => {
-        fetchUf();
-    }, []);
+  useEffect(() => {
+    fetchUf();
+  }, []);
 
-    return (
-        <div className="App">
-            {notificationContextHandler}
-            <Header />
-            <TrilhaDeNavegacao elementosLi={paginas} />
-            <div className="d-flex flex-column m-5">
-                <div className="d-flex justify-content-left align-items-center mr-5">
-                    <InputFilter onChange={setEscola} dataTestId="filtroEscola" label="Escola" placeholder="Escola" />
-                    <Select items={listaUfs} value={uf} label={"UF:"} onChange={setUf} dropdownStyle={{ marginLeft: "20px", width: "260px" }} filtrarTodos={true} />
-                    <Select items={listaMunicipios} value={municipio} label={"Municipios:"} onChange={setMunicipio} dropdownStyle={{ marginLeft: "20px", width: "260px" }} filtrarTodos={true} />
-                </div>
-                {listaSolicitacoes.length === 0 && <Table columsTitle={['Escola', 'Qtd. de Alunos', 'UF', 'Municipio']} initialItemsPerPage={10} title="Usuários cadastrados"><></><></></Table>}
-
-                <Table columsTitle={['Escola', 'Qtd. de Alunos', 'UF', 'Municipio']} initialItemsPerPage={10} title="Soliciatações de Ações">
-                    {
-                        listaSolicitacoes.map((usuario, index) =>
-                            <CustomTableRow key={`${usuario.id}-${index}`} id={index}
-                                data={{ '0': usuario.nome, '1': `${temPermissao(Permissao.PerfilVisualizar) ? usuario.perfil.nome : ""}`, '2': `${procuraRotuloUf(usuario)}`, '3': `${usuario.email}` }}
-                                hideEyeIcon={false}
-                                hideTrashIcon={false}
-                                onDeleteRow={() => { }}
-                                onDetailRow={() => { }}
-                            />
-                        )
-                    }
-                </Table>
-                {loading && <div className="d-flex justify-content-center w-100 m-5"><ReactLoading type="spinningBubbles" color="#000000" /></div>}
-            </div>
-            <Footer />
+  return (
+    <div className="App">
+      {notificationContextHandler}
+      <Header />
+      {<SolicitacoesDialog onClose={() => { setSolicitacaoAtual(null) }} onCreateAcao={() => { }} />}
+      <TrilhaDeNavegacao elementosLi={paginas} />
+      <div className="d-flex flex-column m-5">
+        <div className="d-flex justify-content-left align-items-center mr-5">
+          <InputFilter onChange={setEscola} dataTestId="filtroEscola" label="Escola" placeholder="Escola" />
+          <Select items={listaUfs} value={uf} label={"UF:"} onChange={setUf} dropdownStyle={{ marginLeft: "20px", width: "260px" }} filtrarTodos={true} />
+          <Select items={listaMunicipios} value={municipio} label={"Municipios:"} onChange={setMunicipio} dropdownStyle={{ marginLeft: "20px", width: "260px" }} filtrarTodos={true} />
         </div>
-    )
+        {listaSolicitacoes.length === 0 && <Table columsTitle={['Escola', 'Qtd. de Alunos', 'UF', 'Municipio']} initialItemsPerPage={10} title="Usuários cadastrados"><></><></></Table>}
+
+        <Table
+          columsTitle={['Escola', 'Qtd. de Alunos', 'UF', 'Municipio']}
+          initialItemsPerPage={tamanhoPagina} 
+          title="Soliciatações de Ações"
+          totalPages={totalPaginas}
+          totalItems={100}
+          onNextPage={() => {
+            if (pagina === totalPaginas) return;
+            buscarSolicitacoes(pagina + 1, tamanhoPagina)
+          }}
+          onPreviousPage={() => {
+            if (pagina === 1) return;
+            buscarSolicitacoes(pagina - 1, tamanhoPagina)
+          }}
+          onPageResize={(newItensPerPage) => {
+            setTamanhoPagina(newItensPerPage)
+          }}
+          onPageSelect={(newSelectedPage) => {
+            setPagina(newSelectedPage)
+          }}
+          >
+          {
+            listaSolicitacoes.map((solicitacao, index) =>
+              <CustomTableRow key={`${solicitacao.id}-${index}`} id={index}
+                data={{
+                  '0': solicitacao.escola.nomeEscola,
+                  '1': `${solicitacao.escola.numeroTotalDeAlunos}`,
+                  '2': `${solicitacao.escola.siglaUf}`,
+                  '3': `${solicitacao.escola.nomeMunicipio}`
+                }}
+                hideEyeIcon={false}
+                hideTrashIcon={false}
+                onDeleteRow={() => { }}
+                onDetailRow={_ => setSolicitacaoAtual(solicitacao)}
+              />
+            )
+          }
+        </Table>
+        {loading && <div className="d-flex justify-content-center w-100 m-5"><ReactLoading type="spinningBubbles" color="#000000" /></div>}
+      </div>
+      <Footer />
+    </div>
+  )
 }
