c0924bbe5bea63495ba3786fe95178acf02a802c
DiceRunner714 <eduardo.sandes6@gmail.com>
2023-12-03 13:11:28 -0300

    test: adiciona testes DetalhesRanqueModal

--- src/tests/DetalhesRanque.spec.tsx
+++ src/tests/DetalhesRanque.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import ModalDetalhesRanque from '../components/DetalhesRanqueModal';
 import formatDate from '../components/DetalhesRanqueModal';
 import { RanqueData } from '../models/ranque';
@@ -45,6 +45,68 @@ describe('test DetalhesRanqueModal', () => {
     });
     expect(getByText(`Descrição do Ranque:`)).toBeInTheDocument();
     expect(getByText(`${mockRanque.descricao}`)).toBeInTheDocument();
+  });
+
+  it('onClose e chamada quando "Fechar" e clicado', () => {
+    const mockRanque: RanqueData = {
+      id: 1,
+      numEscolas: 5,
+      data: '2023-12-01T08:00:00.000Z',
+      descricao: 'Descrição do ranque mock',
+      fatores: [
+        { nome: 'Fator 1', peso: 0.5, valor: 10 },
+        { nome: 'Fator 2', peso: 0.3, valor: 8 },
+      ],
+    };
+    const onCloseMock = jest.fn();
+
+    const { getByText } = render(
+      <ModalDetalhesRanque
+        ranque={mockRanque}
+        onClose={onCloseMock}
+        onEditDescription={jest.fn()}
+      />
+    );
+
+    fireEvent.click(getByText('Fechar'));
+
+    expect(onCloseMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('alterna descrição entre modo de edição e exibição', async () => {
+    const mockRanque: RanqueData = {
+      id: 1,
+      numEscolas: 5,
+      data: '2023-12-01T08:00:00.000Z',
+      descricao: 'Descrição do ranque mock',
+      fatores: [
+        { nome: 'Fator 1', peso: 0.5, valor: 10 },
+        { nome: 'Fator 2', peso: 0.3, valor: 8 },
+      ],
+    };
+
+    const { getByText } = render(
+      <ModalDetalhesRanque
+        ranque={mockRanque}
+        onClose={jest.fn()}
+        onEditDescription={jest.fn()}
+      />
+    );
+
+    const editarDescricao = getByText('Editar Descrição');
+    fireEvent.click(editarDescricao);
+
+    await waitFor(() => {
+      const textArea = document.querySelector('textarea');
+      expect(textArea).toBeInTheDocument();
+    });
+
+    fireEvent.click(getByText('Salvar Descrição'));
+
+    await waitFor(() => {
+      const labelElement = document.querySelector('label');
+      expect(labelElement).toBeInTheDocument();
+    });
   });
 });
  
