64481596b9fc46ca8018466e5b9d12edeac1c073
Hunter104 <109833229+Hunter104@users.noreply.github.com>
2023-12-04 10:55:45 -0300

    test: adiciona mocks de listagem e cadastro de polos em servicosAPI.ts

--- src/tests/mock/servicosAPI.ts
+++ src/tests/mock/servicosAPI.ts
@@ -1,6 +1,23 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import { atualizarTipoPerfil, atualizarTokenUrl, cadastrarPerfilUrl, excluiPerfil, listarEscolasRanque, listarPerfis, listarPermissoesCategoria, listarUsuarioPermissoes, listarUsuarios, municipioURL, obterPerfil, ranqueamentoProcessamento, unidadesFederativasURL, urlAPIEscolas, urlAPIUps } from "../../consts/service";
+import {
+    atualizarTipoPerfil,
+    atualizarTokenUrl,
+    cadastrarPerfilUrl,
+    excluiPerfil,
+    listarEscolasRanque,
+    listarPerfis,
+    listarPermissoesCategoria,
+    listarUsuarioPermissoes,
+    listarUsuarios,
+    municipioURL,
+    obterPerfil, polosFiltradosUrl,
+    poloUrl,
+    ranqueamentoProcessamento,
+    unidadesFederativasURL,
+    urlAPIEscolas,
+    urlAPIUps
+} from "../../consts/service";
 import empresaRequests from "./empresa/API";
 import { Permissao, TipoPerfil } from "../../models/auth";
 import { usuarios } from "../stub/usuarioModelos";
@@ -10,6 +27,72 @@ const escolasService = urlAPIEscolas;
 const upsService = urlAPIUps;
 
 const server = setupServer(
+    rest.get(polosFiltradosUrl, (_, res, ctx) => {
+        return res(ctx.json(
+            {
+                "pagina": 1,
+                "itemsPorPagina": 3,
+                "total": 3,
+                "totalPaginas": 1,
+                "items": [
+                    {
+                        "id": 2,
+                        "endereco": "R. Sampaio Marquês, 22 - Pajuçara, Maceió - AL",
+                        "cep": "57030-160",
+                        "latitude": "-9,6739546",
+                        "nome": "Superintendência regional do DNIT em Alagoas",
+                        "municipio": {
+                            "nome": "Maceió",
+                            "id": 2704302
+                        },
+                        "longitude": "-35,7181362",
+                        "uf": {
+                            "nome": "Alagoas",
+                            "id": 2,
+                            "sigla": "AL"
+                        }
+                    },
+                    {
+                        "id": 7,
+                        "endereco": "R. Afonso Sarlo, 2340 - Bento Ferreira, Vitória - ES",
+                        "cep": "29050-790",
+                        "latitude": "-20,3178888",
+                        "nome": "Superintendência regional do DNIT em Espírito Santo",
+                        "municipio": {
+                            "nome": "Vitória",
+                            "id": 3205309
+                        },
+                        "longitude": "-40,3055323",
+                        "uf": {
+                            "nome": "Espírito Santo",
+                            "id": 7,
+                            "sigla": "ES"
+                        }
+                    },
+                    {
+                        "id": 8,
+                        "endereco": "Av. 24 de Outubro, 311 - St. dos Funcionários, Goiânia - GO",
+                        "cep": "74543-100",
+                        "latitude": "-16,6726216",
+                        "nome": "Superintendência regional do DNIT em Goiás",
+                        "municipio": {
+                            "nome": "Goiânia",
+                            "id": 5208707
+                        },
+                        "longitude": "-49,2823321",
+                        "uf": {
+                            "nome": "Goiás",
+                            "id": 8,
+                            "sigla": "GO"
+                        }
+                    },
+                ]
+            }
+        ))
+    }),
+  rest.post(poloUrl, (_, res, ctx) => res(
+    ctx.status(200)
+  )),
   rest.get(
     listarUsuarioPermissoes,
     (_, res, ctx) => res(ctx.json(Object.values(Permissao))),
