487427be7c50afa2aa62692c98f042c1072f6c0c
bottinolucas <lucasgabottino@gmail.com>
2023-12-06 14:06:52 -0300

    test: verifica se sobe erro de quantidade mínima de caracteres Co-authored-by: João G. Carvalho <joaoseisei@gmail.com>

--- src/tests/GerarPlanejamento.spec.tsx
+++ src/tests/GerarPlanejamento.spec.tsx
@@ -5,6 +5,7 @@ import server from "./mock/servicosAPI";
 import { Permissao } from "../models/auth";
 import { autenticar } from "./mock/autenticacao";
 import GerenciarAcoes from "../pages/gerencia/GerenciarAcoes/GerarPlanejamento";
+import { act } from "react-dom/test-utils";
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
@@ -56,5 +57,28 @@ describe("Criar planejamento", () => {
     }
 
   })
-  
+
+  it("Deve subir erro caso a string passada na Label Título tiver menos que 5 caracteres", async() => {
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <GerenciarAcoes/>
+        </AuthProvider>
+      </MemoryRouter>
+    );
+
+    const button = screen.getByText("Gerar Planejamento")
+    const string = screen.getByTestId("inputTitulo");
+
+    act(() => {
+      fireEvent.change(string, {target: {value: "abcd"}})
+    })
+
+    const stringHTML = string.outerHTML;
+
+    fireEvent.click(button);
+    if(stringHTML.length < 5) {
+      await waitFor(() => expect(screen.getByText('O nome do planejamento deve conter pelo menos 5 caracteres')).toBeInTheDocument());
+    }
+  })
 })
