d2e8c654c93baf4d95ed46c9e8e79160b8792859
Cássio Reis <csreis04@gmail.com>
2023-12-06 11:52:22 -0300

    feat: adiciona tabela para parâmetros de custo logístico

--- src/pages/gerencia/GerenciarPrioridades/index.tsx
+++ src/pages/gerencia/GerenciarPrioridades/index.tsx
@@ -20,7 +20,43 @@ export default function GerenciarPrioridades() {
             key: '2',
             label: "Custo Logístico",
             children: (
-                <FatorForm nome="Custo Logístico" primario></FatorForm>
+                <div className="custo-logistico">
+                    <FatorForm nome="Custo Logístico" primario></FatorForm>
+                    <div className="custo-table">
+                        <p>Parâmetros do Custo Logístico</p>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Custo</th>
+                                    <th>Raio</th>
+                                    <th>Valor</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>$</th>
+                                    <th>0 - <input className="br-input small"></input></th>
+                                    <th>50</th>
+                                </tr>
+                                <tr>
+                                    <th>$$</th>
+                                    <th><input className="br-input small"></input> - <input className="br-input small"></input></th>
+                                    <th>50</th>
+                                </tr>
+                                <tr>
+                                    <th>$$$</th>
+                                    <th><input className="br-input small"></input> - <input className="br-input small"></input></th>
+                                    <th>50</th>
+                                </tr>
+                                <tr>
+                                    <th>$$$$</th>
+                                    <th>Acima de XXX</th>
+                                    <th>50</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             )
         },
         {
