be8cad6aa45bdaf393a713881482a82eaf219a84
bottinolucas <lucasgabottino@gmail.com>
2023-12-08 12:25:35 -0300

    feat: implementando deleteEscola
    Co-authored-by: Vitor Lamego <vitorlamegoo@gmail.com>
    Co-authored-by: João G. Carvalho <joaoseisei@gmail.com>
    Co-authored-by: Daniel Porto <danielportods@gmail.com>

--- src/service/gerenciarAcoes.ts
+++ src/service/gerenciarAcoes.ts
@@ -2,15 +2,29 @@ import {fetchDados, ResponseStatus, sendCadastros} from "./apiUtils";
 import * as DATA from "../models/service";
 import * as URL from "../consts/service"
 import {PlanejamentoMacro} from "../models/gerenciarAcoes";
+import axios, { AxiosResponse } from "axios";
 
+//envia cadastro de planejamento macro
 export async function sendPlanejamento(data: DATA.CriarPlanejamentoRequest): Promise<ResponseStatus> {
     return sendCadastros<DATA.CriarPlanejamentoRequest>(URL.criaPlanejamento, data);
 }
-
+//busca lista de planejamentos macro
 export async function fetchPlanejamento(){
     return fetchDados<PlanejamentoMacro[]>(`${URL.criaPlanejamento}`);
 }
-
+//busca planejamento macro específico por id
 export async function fetchPlanejamentoId(id: string): Promise<PlanejamentoMacro> {
     return fetchDados<PlanejamentoMacro>(`${URL.criaPlanejamento}/${id}`);
+}
+
+export async function deletePlanejamentoMacro({id_planejamento}: DATA.ExcluirPlanejamentoData): Promise<ResponseStatus>{
+    try{
+        const response: AxiosResponse<ResponseStatus> = await axios.delete(
+            URL.excluiPlanejamento, { params: { id: id_planejamento}}
+        );
+        return response.data;
+    } catch (error){
+        console.error(error);
+        throw error;
+    }
 }
