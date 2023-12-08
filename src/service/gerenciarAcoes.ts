f7e65191d5860a3cf1efee669fbbfa521737a386
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2023-12-08 12:14:25 -0300

    feat: adiciona fetchPlanejamento
    Co-authored-by: Lucas Bottino <lucasgabottino@gmail.com>

--- src/service/gerenciarAcoes.ts
+++ src/service/gerenciarAcoes.ts
@@ -1,7 +1,12 @@
-import { ResponseStatus, sendCadastros } from "./apiUtils";
+import {fetchDados, ResponseStatus, sendCadastros} from "./apiUtils";
 import * as DATA from "../models/service";
 import * as URL from "../consts/service"
+import {PlanejamentoMacro} from "../models/gerenciarAcoes";
 
 export async function sendPlanejamento(data: DATA.CriarPlanejamentoRequest): Promise<ResponseStatus> {
     return sendCadastros<DATA.CriarPlanejamentoRequest>(URL.criaPlanejamento, data);
+}
+
+export async function fetchPlanejamento(){
+    return fetchDados<PlanejamentoMacro[]>(`${URL.criaPlanejamento}`);
 }
