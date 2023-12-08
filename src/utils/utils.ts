aa419480c7e92816bba5a4020bc9018235b3dd27
VitorLamego <vitorlamegoo@gmail.com>
2023-12-08 00:32:18 -0300

    feat: create utils and fixtures to help methods
    Co-authored-by: Daniel Porto <danielportods@gmail.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,3 +10,16 @@ export function formataCustoLogistico(distancia: number){
             return "$$$$"
     }
 }
+
+export function numeroCustoLogistico(distancia: number){
+    switch(true){
+        case distancia <= 250:
+            return 1;
+        case distancia > 250 && distancia <= 500:
+            return  2
+        case distancia > 500 && distancia <= 1000:
+            return 3
+        default:
+            return 4
+    }
+}
