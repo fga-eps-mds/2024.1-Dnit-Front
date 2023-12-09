1deafbd27d0103df1e72c7cc4a02442feb5c1fc6
Cássio Reis <csreis04@gmail.com>
2023-12-09 18:35:17 -0300

    test: adiciona teste de renderização do componente FatorForm

---
+++ src/tests/components/FatorForm.spec.tsx
@@ -0,0 +1,22 @@
+import { MemoryRouter } from "react-router-dom";
+import FatorForm from "../../components/FatorForm";
+import server from "../mock/servicosAPI";
+import { render } from "@testing-library/react";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("Testes para componente FatorForm", () => {
+    it("Deve renderizar o componente", () => {
+        const screen = render(
+            <MemoryRouter>
+                <FatorForm nome="Teste"></FatorForm>
+            </MemoryRouter>
+        )
+
+        expect(screen.getByText("Teste")).toBeInTheDocument();
+        expect(screen.getByText("Cancelar")).toBeInTheDocument();
+        expect(screen.getByText("Salvar")).toBeInTheDocument();
+    })
+}) 
