ab86da5a776ab85160d4773db3f03404b2142336
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2023-12-10 13:00:30 -0300

    fix: alterando o modal alterar escola

--- src/components/GerenciarAcoesModal/AlterarEscola/index.tsx
+++ src/components/GerenciarAcoesModal/AlterarEscola/index.tsx
@@ -1,64 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Modal from "../../Modal";
 import ReactLoading from "react-loading";
+import {EscolaData} from "../../../models/service";
 
 interface ModalProps {
     //escolaId: string
     onClose: () => void;
-    //onSalvar: () => void;
 }
 
-//TODO VERIFICAR QUAIS OUTROS PARAMETROS VAI TER
-interface Escola {
-    id: number
-    nome: string;
-    uf: string;
-    qtdAlunos: number;
-}
-
-const dados: Escola = { id: 1, nome: "Sigma", uf: "SP", qtdAlunos: 500};
 
 const ModalAlterarEscola: React.FC<ModalProps> = ({ onClose }) => {
     
-    const [escola, setEscola] = useState<Escola | null>(dados);
-
-    //TODO A FUNCAO FETCHESCOLA PELO ID
-    // useEffect(() => {
-    //     fetchEscolas(escolaId)
-    //         .then((escola) => {
-    //                 setEscola(escola);
-    //             }
-    //         )
-    // }, []);
-
-    if(!escola){
-        return (
-            <Modal className="modal-title" closeModal={() => onClose()}>
-                <h4 className="text-center mt-2">Carregando Escola...</h4>
-                <div className="d-flex justify-content-center m-4">
-                    <ReactLoading type="spinningBubbles" color="#000000" />
-                </div>
-                <span></span>
-            </Modal>
-        );
-    }
-
+    const [escola, setEscola] = useState<EscolaData | null>(null);
+    
     return (
-        <Modal className="default escola-ranque-modal" closeModal={() => onClose()}>
-            <div className="d-flex flex-column">
-                <div>
-                    <h4 className="text-center mt-1">Alterar Escola</h4>
-                </div>
-                
-                <div className="d-flex w-100 justify-content-end mb-2">
-                    <button className="br-button secondary mr-3" type="button" onClick={() => onClose()}>
-                        Cancelar
-                    </button>
-                    <button className="br-button primary mr-3" type="button" disabled>
-                        Salvar
-                    </button>
-                </div>
+        <Modal className="modal-title" closeModal={() => onClose()}>
+            <h4 className="text-center mt-2">Indisponível...</h4>
+            <div className="d-flex justify-content-center m-4">
+                <ReactLoading type="spinningBubbles" color="#000000" />
             </div>
+            <span></span>
         </Modal>
     );
 };
