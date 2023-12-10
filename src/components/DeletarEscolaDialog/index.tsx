93f9bbda5c3989a4781ce326662af7711fb0d405
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2023-12-10 16:48:57 -0300

    fix: corrige modal de deletar escola

--- src/components/DeletarEscolaDialog/index.tsx
+++ src/components/DeletarEscolaDialog/index.tsx
@@ -1,31 +1,27 @@
 import { useState } from "react";
 import Modal from "../Modal";
-import { deleteEscolaPlanejamento } from "../../service/gerenciarAcoes";
+import {deleteEscolaPlanejamento, updatePlanejamento} from "../../service/gerenciarAcoes";
 import { notification } from "antd";
 import "./styles.css"
+import {
+    EscolaPlanejamentoModel,
+    EscolasPlanejamentoTabela,
+    InfoMesPlanejamentoMacro,
+    PlanejamentoMacro
+} from "../../models/gerenciarAcoes";
 export interface DeletarEscolaDialogArgs {
   id: string;
   nome: string;
 }
 
 interface DeletarEscolaDialogProps {
-  readonly id: string;
-  readonly nome: string;
-  readonly closeDialog: (deleted : boolean) => void
+    readonly planejamento: PlanejamentoMacro
+    readonly escola: EscolasPlanejamentoTabela | undefined;
+    readonly infoMes: InfoMesPlanejamentoMacro | undefined;
+    readonly closeDialog: (deleted : boolean) => void
 }
 
-export default function DeletarEscolaDialog( { id, nome, closeDialog} : DeletarEscolaDialogProps) {
-  const deletarEscola = () => {
-    deleteEscolaPlanejamento(id).then(() => {
-      notification.success({ message: "Escola deletada com sucesso!"});
-      closeDialog(true);
-    })
-    .catch((error) => {
-      notification.error({
-        message: "Falha na exclusão da escola" + (error?.response?.data ?? "")
-      })
-    }) 
-  }
+export default function DeletarEscolaDialog( { planejamento, infoMes, escola, closeDialog} : DeletarEscolaDialogProps) {
 
   return (
     <Modal className="delete-escola-planejamento" closeModal={() => closeDialog(false)}>
@@ -33,15 +29,47 @@ export default function DeletarEscolaDialog( { id, nome, closeDialog} : DeletarE
         <strong>Tem certeza que deseja excluir esta escola?</strong>
       </p>
       <p>
-        A escola <strong>{nome}</strong> será excluída do planejamento.
+        A escola <strong>{escola?.nome}</strong> será excluída do planejamento.
       </p>
-      <div className="d-flex w-100 justify-content-center">
+      <div className="d-flex w-100 justify-content-end mb-2">
         <button className="br-button secondary" type="button" onClick={() => closeDialog(false)} data-testid="botaoCancelar">
           Cancelar
        </button>
-        <button className="br-button primary" type="button"onClick={deletarEscola} data-testid="botaoConfirmar">
-          Confirmar
-        </button>
+          <button
+              className="br-button primary"
+              type="button"
+              data-testid="botaoConfirmar"
+              onClick={() => {
+                  const newInfoPlanejamentoMacro = planejamento?.planejamentoMacroMensal.map(p => {
+                      if (p === infoMes) {
+                          const escolasFiltradas = p?.escolas.filter(e => (
+                              escola?.nome !== e.nome &&
+                              escola?.quantidadeAlunos !== e.quantidadeAlunos &&
+                              escola?.uf !== e.uf
+                          ));
+                          return { ...p, escolas: escolasFiltradas };
+                      }
+                      return p;
+                  });
+
+                  var newPlanejamento = planejamento;
+                  newPlanejamento.planejamentoMacroMensal = newInfoPlanejamentoMacro;
+
+                  if (newPlanejamento) {
+                      updatePlanejamento(planejamento.id, newPlanejamento)
+                          .then(() => {
+                              notification.success({ message: "Escolas Deletada do Planejamento com Sucesso!" });
+                          })
+                          .catch((error) => {
+                              notification.error({
+                                  message: "Falha ao Deletar Escolas do Planejamento."
+                              });
+                          });
+                  }
+              }}
+          >
+              Confirmar
+          </button>
       </div>
     </Modal>
   )
