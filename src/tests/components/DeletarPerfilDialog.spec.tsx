8e8bad65132139cd858e1bf42ba3c9d2a1019727
VitorLamego <vitorlamegoo@gmail.com>
2023-12-11 13:26:14 -0300

    hotfix: corrige mock de requisição no teste falhando

--- src/tests/components/DeletarPerfilDialog.spec.tsx
+++ src/tests/components/DeletarPerfilDialog.spec.tsx
@@ -6,6 +6,14 @@ import { MemoryRouter } from "react-router-dom";
 import { autenticar } from "../mock/autenticacao";
 import { Permissao } from "../../models/auth";
 import server from "../mock/servicosAPI";
+import { deletePerfil } from "../../service/usuarioApi";
+
+jest.mock("../../service/usuarioApi", () => ({
+  ...jest.requireActual("../../service/usuarioApi"),
+  deletePerfil: jest.fn(),
+}));
+
+const mockedDeletePerfil = deletePerfil as jest.Mock;
 
 beforeAll(() => {
   server.listen();
@@ -15,85 +23,130 @@ afterEach(() => {
   cleanup();
 });
 
-describe('DeletarPerfilDialog', () => {
-  test('deve renderizar', () => {
+describe("DeletarPerfilDialog", () => {
+  test("deve renderizar", () => {
     const screen = render(
       <MemoryRouter>
-        <DeletarPerfilDialog onClose={() => { }} perfil={{ id: '1', nome: 'perfil', quantidade: 1 }} />
+        <DeletarPerfilDialog
+          onClose={() => {}}
+          perfil={{ id: "1", nome: "perfil", quantidade: 1 }}
+        />
       </MemoryRouter>
     );
 
-    screen.getByText('Tem certeza que deseja excluir esse perfil?');
-    screen.getByText('O perfil perfil é usado por 1 usuários.');
-    screen.getByText('Cancelar');
-    screen.getByText('Confirmar');
+    screen.getByText("Tem certeza que deseja excluir esse perfil?");
+    screen.getByText("O perfil perfil é usado por 1 usuários.");
+    screen.getByText("Cancelar");
+    screen.getByText("Confirmar");
   });
 
-  test('deve cancelar', () => {
+  test("deve cancelar", () => {
     autenticar(Permissao.PerfilRemover);
     let deletou = true;
 
     const screen = render(
       <MemoryRouter>
-        <DeletarPerfilDialog onClose={d => deletou = d} perfil={{ id: '1', nome: 'perfil', quantidade: 1 }} />
+        <DeletarPerfilDialog
+          onClose={(d) => (deletou = d)}
+          perfil={{ id: "1", nome: "perfil", quantidade: 1 }}
+        />
       </MemoryRouter>
     );
 
-    screen.getByText('Cancelar').click();
+    screen.getByText("Cancelar").click();
     expect(deletou).toBe(false);
   });
 
-  test('deve mostrar carregando', async () => {
+  test("deve mostrar carregando", async () => {
+    mockedDeletePerfil.mockImplementation(
+      () =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve({ success: true });
+          }, 2000);
+        })
+    );
     autenticar(Permissao.PerfilRemover);
     const screen = render(
       <MemoryRouter>
-        <DeletarPerfilDialog onClose={() => { }} perfil={{ id: '1', nome: 'perfil', quantidade: 1 }} />
+        <DeletarPerfilDialog
+          onClose={() => {}}
+          perfil={{ id: "1", nome: "perfil", quantidade: 1 }}
+        />
       </MemoryRouter>
     );
 
-    screen.getByText('Confirmar').click();
-    await waitFor(() => expect(screen.getByText('Deletando perfil perfil...')).toBeInTheDocument());
+    screen.getByText("Confirmar").click();
+    await waitFor(() =>
+      expect(screen.getByText("Deletando perfil perfil...")).toBeInTheDocument()
+    );
   });
 
-  test('deve excluir', async () => {
+  test("deve mostrar notificacão de erro", async () => {
+    mockedDeletePerfil.mockRejectedValue(new Error("Erro na requisição"));
+
     let deletou = false;
     const screen = render(
       <MemoryRouter>
-        <DeletarPerfilDialog onClose={(d) => deletou = d} perfil={{ id: '1', nome: 'perfil', quantidade: 1 }} />
+        <DeletarPerfilDialog
+          onClose={(d) => (deletou = d)}
+          perfil={{ id: "erro", nome: "perfil", quantidade: 1 }}
+        />
       </MemoryRouter>
     );
 
-    const confirmar = screen.getByText('Confirmar');
-    fireEvent.click(confirmar);
-    await waitFor(() => expect(screen.getByText('Deletando perfil perfil...')).toBeInTheDocument());
-    await waitFor(() => expect(screen.queryAllByText('Perfil deletado com sucesso').length).toBeGreaterThan(0));
-    expect(deletou).toBeTruthy();
-  });
-
-  test('deve mostrar notificacão de erro', async () => {
-    let deletou = false;
-    const screen = render(
-      <MemoryRouter>
-        <DeletarPerfilDialog onClose={(d) => deletou = d} perfil={{ id: 'erro', nome: 'perfil', quantidade: 1 }} />
-      </MemoryRouter>
+    screen.getByText("Confirmar").click();
+    await waitFor(() =>
+      expect(
+        screen.getByText("Falha na exclusão do perfil.")
+      ).toBeInTheDocument()
     );
-
-    screen.getByText('Confirmar').click();
-    await waitFor(() => expect(screen.getByText('Falha na exclusão do perfil.')).toBeInTheDocument());
     expect(deletou).not.toBeTruthy();
   });
 
-  test('deve fechar ao clikar no overlay', async () => {
+  test("deve fechar ao clikar no overlay", async () => {
     const screen = render(
       <MemoryRouter>
-        <DeletarPerfilDialog onClose={() => { }} perfil={{ id: '1', nome: 'perfil', quantidade: 1 }} />
+        <DeletarPerfilDialog
+          onClose={() => {}}
+          perfil={{ id: "1", nome: "perfil", quantidade: 1 }}
+        />
       </MemoryRouter>
     );
 
-    await waitFor(expect(screen.getByText('Tem certeza que deseja excluir esse perfil?')).toBeInTheDocument);
+    await waitFor(
+      expect(screen.getByText("Tem certeza que deseja excluir esse perfil?"))
+        .toBeInTheDocument
+    );
 
-    const overlay = screen.getByTestId('overlay');
-    fireEvent.click(overlay); 
+    const overlay = screen.getByTestId("overlay");
+    fireEvent.click(overlay);
   });
 });
 
+test("deve excluir", async () => {
+  mockedDeletePerfil.mockResolvedValue({ success: true });
+  let deletou = false;
+  const screen = render(
+    <MemoryRouter>
+      <DeletarPerfilDialog
+        onClose={(d) => (deletou = d)}
+        perfil={{ id: "1", nome: "perfil", quantidade: 1 }}
+      />
+    </MemoryRouter>
+  );
+
+  const confirmar = screen.getByText("Confirmar");
+  fireEvent.click(confirmar);
+  await waitFor(() =>
+    expect(screen.getByText("Deletando perfil perfil...")).toBeInTheDocument()
+  );
+  await waitFor(() =>
+    expect(
+      screen.queryAllByText("Perfil deletado com sucesso").length
+    ).toBeGreaterThan(0)
+  );
+  expect(deletou).toBeTruthy();
+
+  mockedDeletePerfil.mockRestore();
+});
